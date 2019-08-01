9107243523784b19dc4345f2fee7611813320b68
KuanHsien <coson002fy@gmail.com>
2019-08-01 14:17:27 +0800

    [MapPolygonActivity] Add Map with clickable polygon and polyline

--- app/src/main/java/com/kuanhsien/app/sample/android_google_map_demo/data/DemoType.kt
+++ app/src/main/java/com/kuanhsien/app/sample/android_google_map_demo/data/DemoType.kt
@@ -3,7 +3,8 @@ package com.kuanhsien.app.sample.android_google_map_demo.data
 import com.kuanhsien.app.sample.android_google_map_demo.R
 
 enum class DemoType(val titleRes: Int) {
-    MapSimple(R.string.title_map_simple_activity)
+    MapSimple(R.string.title_map_simple_activity),
+    MapWithPolygon(R.string.title_map_polygon_activity)
     ;
 
     companion object {
