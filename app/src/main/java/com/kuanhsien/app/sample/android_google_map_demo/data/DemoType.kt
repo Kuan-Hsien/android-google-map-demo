9ebcaae45a6c797d00e472c870d91307c5173699
KuanHsien <coson002fy@gmail.com>
2019-08-08 11:31:23 +0800

    [ClusterCustomMarkerDemoActivity] Add ClusterCustomMarkerDemoActivity for custom cluster marker render demo 1. Add ClusterCustomMarkerDemoActivity 2. Add PersonClusterItem 3. Add MultiDrawable

--- app/src/main/java/com/kuanhsien/app/sample/android_google_map_demo/data/DemoType.kt
+++ app/src/main/java/com/kuanhsien/app/sample/android_google_map_demo/data/DemoType.kt
@@ -2,16 +2,14 @@ package com.kuanhsien.app.sample.android_google_map_demo.data
 
 import androidx.appcompat.app.AppCompatActivity
 import com.kuanhsien.app.sample.android_google_map_demo.R
-import com.kuanhsien.app.sample.android_google_map_demo.ui.map.ClusterDemoActivity
-import com.kuanhsien.app.sample.android_google_map_demo.ui.map.MarkerRetapCloseInfoDemoActivity
-import com.kuanhsien.app.sample.android_google_map_demo.ui.map.MarkerDemoActivity
-import com.kuanhsien.app.sample.android_google_map_demo.ui.map.PolygonDemoActivity
+import com.kuanhsien.app.sample.android_google_map_demo.ui.map.*
 
 enum class DemoType(val titleRes: Int, val activityClass: Class<out AppCompatActivity>) {
 
     MarkerDemo(R.string.title_activity_marker_demo, MarkerDemoActivity::class.java),
     MarkerRetapCloseInfoDemo(R.string.title_activity_marker_retap_close_info_demo, MarkerRetapCloseInfoDemoActivity::class.java),
     ClusterDemo(R.string.title_activity_cluster_demo, ClusterDemoActivity::class.java),
+    ClusterCustomMarkerDemo(R.string.title_activity_cluster_custom_marker_demo, ClusterCustomMarkerDemoActivity::class.java),
     PolygonDemo(R.string.title_activity_polygon_demo, PolygonDemoActivity::class.java)
     ;
 
---
+++ app/src/main/java/com/kuanhsien/app/sample/android_google_map_demo/ui/common/Constant.kt
@@ -0,0 +1,3 @@
+package com.kuanhsien.app.sample.android_google_map_demo.ui.common
+
+const val TAG_DEMO = "[TAG_DEMO]"
